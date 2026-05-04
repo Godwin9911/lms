ded0d4efd8253e951b147729be356b3a9a81575b
Agedah Godwin <agedah99@gmail.com>
2026-05-04 12:54:40 +0100

    phantom

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-set -e
+#!bin/bash
 
-BENCH_DIR="/home/frappe/frappe-bench"
-
-echo "🧨 Removing old bench if it exists..."
-rm -rf "$BENCH_DIR"
-
-echo "🚀 Creating new bench..."
+if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+    echo "Bench already exists, skipping init"
+    cd frappe-bench
+    bench start
+else
+    echo "Creating new bench..."
+fi
 
 export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
 
@@ -14,11 +14,13 @@ bench init --skip-redis-config-generation frappe-bench
 
 cd frappe-bench
 
+# Use containers instead of localhost
 bench set-mariadb-host mariadb
 bench set-redis-cache-host redis://redis:6379
 bench set-redis-queue-host redis://redis:6379
 bench set-redis-socketio-host redis://redis:6379
 
+# Remove redis, watch from Procfile
 sed -i '/redis/d' ./Procfile
 sed -i '/watch/d' ./Procfile
 
@@ -27,18 +29,16 @@ bench get-app lms
 bench get-app wiki
 
 bench new-site lms.localhost \
-  --force \
-  --mariadb-root-password 123 \
-  --admin-password admin \
-  --no-mariadb-socket
+--force \
+--mariadb-root-password 123 \
+--admin-password admin \
+--no-mariadb-socket
 
 bench --site lms.localhost install-app payments
 bench --site lms.localhost install-app lms
 bench --site lms.localhost install-app wiki
-
 bench --site lms.localhost set-config developer_mode 1
 bench --site lms.localhost clear-cache
-
 bench use lms.localhost
 
 bench start
